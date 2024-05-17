4d8d6d93f6f93fab1d22d953e2f52938bea561ee
Hubert Sleszynski <h.r.sleszynski@gmail.com>
2024-05-17 11:15:56 +0200

    added header & nav sections

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
+import Header from "@/components/Header";
 import StyleGuide from "@/components/StyleGuide";
 
 export default function Home() {
   return (
     <main className="w-full max-w-[1440px] bg-white mx-auto overflow-hidden">
       {/* <StyleGuide /> */}
+      <Header />
       <Hero />
       <div className="h-[4000px]"></div>
     </main>
